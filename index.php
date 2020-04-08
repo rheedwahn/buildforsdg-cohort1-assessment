800c3c886d3bad2e08ce794b5ff9a9bdfb2c1451
rheedwahn <ridwan2bus@gmail.com>
2020-04-08 17:45:44 +0100

    sdg challenge

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+include ('src/estimator.php');
+
+const ESTIMATOR_ROUTE = '/api/v1/on-covid-19';
+
+const LOG_ROUTE = '/api/v1/on-covid-19/logs';
+
+const ESTIMATOR_ROUTE_JSON = '/api/v1/on-covid-19/json';
+
+const ESTIMATOR_ROUTE_XML = '/api/v1/on-covid-19/xml';
+
+header("Access-Control-Allow-Origin: *");
+header("HTTP/1.1 200 OK");
+
+$request_url = $_SERVER['REQUEST_URI'];
+$request_method = $_SERVER['REQUEST_METHOD'];
+$host = $_SERVER['HTTP_HOST'];
+$port = $_SERVER['SERVER_PORT'];
+
+if(in_array($request_url, [ESTIMATOR_ROUTE, ESTIMATOR_ROUTE_JSON]) && $request_method === "POST") {
+    header("Content-Type: application/json; charset=UTF-8");
+    try {
+        echo json_encode(covid19ImpactEstimator(json_decode(file_get_contents("php://input"), true)),
+                        JSON_PRETTY_PRINT);
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 200, $request_method, $request_url),
+                            FILE_APPEND);
+    }catch (Exception $e) {
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 500, $request_method, $request_url),
+                            FILE_APPEND);
+    }
+}
+
+if($request_url === ESTIMATOR_ROUTE_XML && $request_method === "POST") {
+    header("Content-Type: text/xml;charset=utf-8");
+    try {
+        echo json_encode(covid19ImpactEstimator(json_decode(file_get_contents("php://input"), true)),
+            JSON_PRETTY_PRINT);
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 200, $request_method, $request_url),
+            FILE_APPEND);
+    }catch (Exception $e) {
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 500, $request_method, $request_url),
+            FILE_APPEND);
+    }
+}
+
+if($request_url === LOG_ROUTE && $request_method === "GET") {
+    try {
+        echo file_get_contents('log.txt');
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 200, $request_method, $request_url),
+                            FILE_APPEND);
+    }catch (Exception $e) {
+        $response_time = pingDomain(($host.$request_url), $port);
+        file_put_contents('log.txt', logContent($response_time, 500, $request_method, $request_url),
+                            FILE_APPEND);
+    }
+}
+
+function logContent($response_time, $response_code, $method, $request_url)
+{
+    return $method."\t\t".$request_url."\t\t".$response_code."\t\t".$response_time."\n";
+}
+
+function pingDomain($domain,$port)
+{
+    $starttime = microtime(true);
+    $file = @fsockopen($domain, $port, $errno, $errstr, 10);
+    $stoptime = microtime(true);
+
+    if (!$file) {
+        $status = -1;  // Site is down
+    } else {
+        fclose($file);
+        $status = ($stoptime - $starttime) * 1000;
+        $status = floor($status);
+    }
+    return $status.' ms';
+}
--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,94 @@
 <?php
 
+const ESTIMATED_DAYS = 30;
+
+const PERCENTAGE_SEVERE_CASE_ESTIMATE = 15;
+
+const PERCENTAGE_HOSPITAL_BED_ESTIMATE = 35;
+
+const PERCENTAGE_ICU = 5;
+
+const PERCENTAGE_VENTILATOR = 2;
+
 function covid19ImpactEstimator($data)
 {
-  return $data;
+    return formatOutput($data);
+}
+
+function formatOutput($data)
+{
+    return [
+        'data' => $data,
+        'impact' => [
+            'currentlyInfected' => $currently_infected = $data['reportedCases'] * 10,
+            'infectionsByRequestedTime' => $infection_impact = calculateInfectionByRequestedTime(
+                                            $currently_infected,
+                                            $data['periodType'], $data['timeToElapse']),
+            'severeCasesByRequestedTime' => $severe_cases = calculateSevereCasesByRequestedTime($infection_impact),
+            'hospitalBedsByRequestedTime' => calculateTotalHospitalBedForCovid19($data['totalHospitalBeds'], $severe_cases),
+            'casesForICUByRequestedTime' => casesForICUByRequestedTime($infection_impact),
+            'casesForVentilatorsByRequestedTime' => casesForVentilatorsByRequestedTime($infection_impact),
+            'dollarsInFlight' => dollarsInFlight($infection_impact, $data['region']['avgDailyIncomeInUSD'],
+                                                                    $data['region']['avgDailyIncomePopulation'])
+        ],
+        'severeImpact' => [
+            'currentlyInfected' => $currently_infected = $data['reportedCases'] * 50,
+            'infectionsByRequestedTime' => $infection_impact = calculateInfectionByRequestedTime(
+                                            $currently_infected,
+                                            $data['periodType'], $data['timeToElapse']),
+            'severeCasesByRequestedTime' => $severe_cases = calculateSevereCasesByRequestedTime($infection_impact),
+            'hospitalBedsByRequestedTime' => calculateTotalHospitalBedForCovid19($data['totalHospitalBeds'], $severe_cases),
+            'casesForICUByRequestedTime' => casesForICUByRequestedTime($infection_impact),
+            'casesForVentilatorsByRequestedTime' => casesForVentilatorsByRequestedTime($infection_impact),
+            'dollarsInFlight' => dollarsInFlight($infection_impact, $data['region']['avgDailyIncomeInUSD'],
+                                                                    $data['region']['avgDailyIncomePopulation'])
+        ]
+    ];
+}
+
+function calculateInfectionByRequestedTime($currently_infected, $period_type, $time_to_elapse)
+{
+    $factor = floor(normalizeTimeToElapse($period_type, $time_to_elapse) / 3);
+    return $currently_infected * pow(2, $factor);
+}
+
+function normalizeTimeToElapse($period_type, $time_to_elapse)
+{
+    switch ($period_type) {
+        case "months" :
+            $normalised_time = $time_to_elapse * 30;
+            break;
+        case "weeks" :
+            $normalised_time = $time_to_elapse * 7;
+            break;
+        default:
+            $normalised_time = $time_to_elapse;
+    }
+    return $normalised_time;
+}
+
+function calculateSevereCasesByRequestedTime($infections_by_requested_time)
+{
+    return floor(((PERCENTAGE_SEVERE_CASE_ESTIMATE / 100) * $infections_by_requested_time));
+}
+
+function calculateTotalHospitalBedForCovid19($hospital_beds, $severe_case)
+{
+    $available_bed_for_covid_19 = floor(((PERCENTAGE_HOSPITAL_BED_ESTIMATE/100) * $hospital_beds));
+    return $available_bed_for_covid_19 - $severe_case;
+}
+
+function casesForICUByRequestedTime($infections)
+{
+    return floor(((PERCENTAGE_ICU/100) * $infections));
+}
+
+function casesForVentilatorsByRequestedTime($infections)
+{
+    return floor(((PERCENTAGE_VENTILATOR/100) * $infections));
+}
+
+function dollarsInFlight($infection, $avg_daily_income, $avg_income_population)
+{
+    return $infection * $avg_daily_income * $avg_income_population * ESTIMATED_DAYS;
 }
